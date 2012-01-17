46198d137a1f031e969e866fd9252492cad701ac
Ahmad Mageed <amageed@gmail.com>
2012-01-17 11:46:15 -0500

    Initial commit

---
+++ Range/Range.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Range
+{
+    public class Range
+    {
+        const string RangePattern = 
+        @"^             # start of string
+        [([]            # character class of opening parens or brackets
+        (?<Start>\d+)   # named group Start, one or more numbers
+        ,               # literal comma
+        (?<End>\d+)     # named group End, one or more numbers
+        [)\]]           # character class of closing parens or bracket
+        $               # end of string";
+
+        #region Properties
+        public string Input { get; private set; }
+        public int Start { get; private set; }
+        public int End { get; private set; }
+
+        public bool IsInclusiveStart
+        {
+            get
+            {
+                return Input.StartsWith("[");
+            }
+        }
+
+        public bool IsInclusiveEnd
+        {
+            get
+            {
+                return Input.EndsWith("]");
+            }
+        }
+
+        public IEnumerable<int> Sequence
+        {
+            get
+            {
+                return GenerateSequence();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Range(string input)
+        {
+            Match m = Regex.Match(input, RangePattern, RegexOptions.IgnorePatternWhitespace);
+            if (m.Success)
+            {
+                Input = input;
+                SetStart(m.Groups["Start"].Value);
+                SetEnd(m.Groups["End"].Value);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+        }
+        #endregion
+
+        #region Methods
+        private void SetStart(string input)
+        {
+            int value = int.Parse(input);
+            if (IsInclusiveStart)
+                Start = value;
+            else
+                Start = value + 1;
+        }
+
+        private void SetEnd(string input)
+        {
+            int value = int.Parse(input);
+            if (IsInclusiveEnd)
+                End = value;
+            else
+                End = value - 1;
+        }
+
+        private IEnumerable<int> GenerateSequence()
+        {
+            for (int i = Start; i <= End; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public bool Contains(Range other)
+        {
+            if (other == null)
+                return false;
+
+            return Start <= other.Start && End >= other.End;
+        }
+
+        public bool Equals(Range other)
+        {
+            if (other == null)
+                return false;
+
+            return Input == other.Input;
+        }
+        #endregion
+    }
+}
---
+++ Range/RangeTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Range
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        [Test]
+        public void CannotInstantiateInvalidRangeTest()
+        {
+            string input = "2,6";
+            Assert.That(() => new Range(input), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void CanCheckInclusiveStartAndEndTest()
+        {
+            string input = "[2,6]";
+            var range = new Range(input);
+
+            Assert.That(range.IsInclusiveStart, Is.True);
+            Assert.That(range.IsInclusiveEnd, Is.True);
+        }
+
+        [Test]
+        public void CanCheckExclusiveStartAndEndTest()
+        {
+            var range = new Range("(2,6)");
+
+            Assert.That(range.IsInclusiveStart, Is.False);
+            Assert.That(range.IsInclusiveEnd, Is.False);
+        }
+
+        [Test]
+        public void CanGetExpectedInclusiveStartAndEndValuesTest()
+        {
+            var range = new Range("[2,6]");
+
+            Assert.That(range.Start, Is.EqualTo(2));
+            Assert.That(range.End, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void CanGetExpectedExclusiveStartAndEndValuesTest()
+        {
+            var range = new Range("(2,6)");
+
+            Assert.That(range.Start, Is.EqualTo(3));
+            Assert.That(range.End, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CanGetInclusiveTwoToSixValuesTest()
+        {
+            var range = new Range("[2,6]");
+
+            int[] expectedResult = { 2, 3, 4, 5, 6 };
+            bool result = expectedResult.SequenceEqual(range.Sequence);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanGetExclusiveTwoToSixValuesTest()
+        {
+            var range = new Range("(2,6)");
+
+            int[] expectedResult = { 3, 4, 5 };
+            bool result = expectedResult.SequenceEqual(range.Sequence);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanGetInclusiveTwoToExclusiveSixValuesTest()
+        {
+            var range = new Range("[2,6)");
+
+            int[] expectedResult = { 2, 3, 4, 5 };
+            bool result = expectedResult.SequenceEqual(range.Sequence);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void RangesAreEqualTest()
+        {
+            string input = "[2,6)";
+            var firstRange = new Range(input);
+            var secondRange = new Range(input);
+
+            bool result = firstRange.Equals(secondRange);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void RangesAreNotEqualTest()
+        {
+            var firstRange = new Range("[2,6)");
+            var secondRange = new Range("(2,6)");
+
+            bool result = firstRange.Equals(secondRange);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void FirstRangeContainsSecondRangeTest()
+        {
+            var firstRange = new Range("[2,10)");
+            var secondRange = new Range("[3,5]");
+
+            bool result = firstRange.Contains(secondRange);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void SecondRangeDoesNotContainFirstRangeTest()
+        {
+            var firstRange = new Range("[3,5]");
+            var secondRange = new Range("[2,10)");
+
+            bool result = firstRange.Contains(secondRange);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IncThreeIncFiveContainsIncThreeExcFiveIsTrueTest()
+        {
+            var firstRange = new Range("[3,5]");
+            var secondRange = new Range("[3,5)");
+
+            bool result = firstRange.Contains(secondRange);
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
